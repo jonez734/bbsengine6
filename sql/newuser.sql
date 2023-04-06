0aff6d63075d7997052b293fed74b2e9c5655839
Jeff MacDonald <jam@zoidtechnologies.com>
2023-04-06 14:33:38 -0400

    - bbsengine6/sql/newuser.sql: removed 'finn' role

--- sql/newuser.sql
+++ sql/newuser.sql
@@ -1,5 +1,5 @@
 grant all on all tables in schema engine to bbs;
-create role finn role bbs;
-alter role finn login;
+--create role finn role bbs;
+--alter role finn login;
 grant usage on schema engine to bbs;
 grant usage on SCHEMA empyre to bbs;
