344e9dcf8b4740f7e8bebc9f416c6ace6c3e28a4
Jeff MacDonald <jam@zoidtechnologies.com>
2023-06-27 16:06:50 -0400

    - bbsengine6/con/__main__.py: added some boilerplate that calls the 'main' submodule

--- py/src/con/__main__.py
+++ py/src/con/__main__.py
@@ -1,7 +1,18 @@
 import ttyio5 as ttyio
 import bbsengine6 as bbsengine
-from bbsengine6 import util
 
-util.title("console")
-buf = ttyio.inputstring("con: ", "")
-print(buf)
+from . import lib
+
+if __name__ == "__main__":
+    bbsengine.screen.init()
+    bbsengine.screen.setarea("con")
+    parser = lib.buildargs()
+    args = parser.parse_args()
+    try:
+        lib.runsubmodule(args, "main")
+    except EOFError:
+        print("EOF")
+    except KeyboardInterrupt:
+        print("INTR")
+    finally:
+        ttyio.echo("{decsc}{curpos:%d,0}{el}{decrc}{reset}{/all}" % (ttyio.getterminalheight()))
