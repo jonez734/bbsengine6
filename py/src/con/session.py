74a3a67569aaf6b2c266ca3c4987ab2ed6d4aff0
Jeff MacDonald <jam@zoidtechnologies.com>
2023-11-01 23:11:25 -0400

    - bbsengine6/py/src/con/session.py: added main()

--- py/src/con/session.py
+++ py/src/con/session.py
@@ -7,6 +7,21 @@ def init(args, **kw):
 def access(args, op, **kw):
     return True
 
-def buildargs(*args, **kw):
+def buildargs(args, **kw):
     return None
-    
+
+def main(args, **kw):
+    bbsengine.util.heading("system sessions summary")
+
+    dbh = bbsengine.database.connect(args)
+    sql = "select * from engine.session order by datecreated"
+    cur = dbh.cursor()
+    cur.execute(sql)
+    if cur.rowcount == 0:
+        ttyio.echo("no sessions exist")
+        return True
+
+    for session in bbsengine.database.resultiter(cur):
+        ttyio.echo(repr(session), level="debug")
+
+    ttyio.echo("----")
