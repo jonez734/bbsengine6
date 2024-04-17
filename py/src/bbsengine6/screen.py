0041f403eced06afd8b24716cb02f0371fcaca36
Jeff MacDonald <jam@zoidtechnologies.com>
2024-04-17 15:45:41 -0400

    - bbsengine6/screen.py: rewrote setarea()

--- py/src/bbsengine6/screen.py
+++ py/src/bbsengine6/screen.py
@@ -28,45 +28,42 @@ def init(topmargin=0, bottommargin=1):
   return
 
 # @since 20230523 copied from bbsengine5
-def setarea(left, right=None, stack:bool=False):
-  global areastack
+def setarea(left, right=None, stack:bool=False, width:int=None):
+    global areastack
 
-  terminalwidth = io.getterminalwidth()-2
+    terminalwidth = width if width is not None else io.getterminalwidth()-2
+#    io.echo(f"{terminalwidth=} {width=}")
 
-  if callable(left):
-    leftbuf = left()
-  elif type(left) == str:
-    leftbuf = left
-  else:
-    leftbuf = type(left) # "ERROR"
+    if callable(left):
+        leftbuf = left()
+    elif type(left) == str:
+        leftbuf = left
+    else:
+        leftbuf = f"{type(left)=}" # "ERROR"
 
-  l = io.interpret(leftbuf, strip=True)
+    l = io.tostr(leftbuf, strip=True, wordwrap=False)
 
-  if callable(right):
-    rightbuf = right()
-  elif type(right) == str:
-    rightbuf = right
-  elif right is None:
-    rightbuf = ""
-  else:
-    io.echo("setarea.100: type(right)=%r" % (right), level="debug")
-    rightbuf = "ERROR" # type(right)
+    if callable(right):
+        rightbuf = right()
+    elif type(right) == str:
+        rightbuf = right
+    elif right is None:
+        rightbuf = ""
+    else:
+        io.echo("setarea.100: type(right)=%r" % (right), level="debug")
+        rightbuf = "ERROR" # type(right)
 
-  r = ""
-  for i in io.interpret(rightbuf, strip=True):
-    r += i
-  t = terminalwidth - len(r) - 4
-  leftbuf = leftbuf[:t] + (leftbuf[t:] and '...')
+    r = io.tostr(rightbuf, strip=True, wordwrap=False)
+#    io.echo(f"{r=}")
+    t = terminalwidth - len(r) - 4
+    leftbuf = leftbuf[:t] + (leftbuf[t:] and '...')
 
-#  ttyio.echo("r=%r rightbuf=%r" % (r, rightbuf), interpret=False)
-
-#  buf = "%s%s" % (ttyio.ljust(leftbuf, terminalwidth-len(r)), rightbuf) # leftbuf.ljust(terminalwidth-len(r), " "), rightbuf)
-  buf = " %s%s " % (leftbuf.ljust(terminalwidth-len(r)), rightbuf)
-  #ttyio.ljust(leftbuf, terminalwidth-len(r)), rightbuf) # leftbuf.ljust(terminalwidth-len(r), " "), rightbuf)
-  updatebottombar("{var:areacolor}%s{/all}" % (buf))
-  if stack is True:
-    areastack.insert(0, buf) # append(buf)
-  return
+    buf = " %s%s " % (leftbuf.ljust(terminalwidth-len(r)-1), rightbuf)
+#    io.echo(f"{buf=} {len(buf)=}")
+    updatebottombar(f"{{areacolor}}{buf}{{/all}}")
+    if stack is True:
+        areastack.insert(0, buf) # append(buf)
+    return
 
 # @since 20230523 copied from bbsengine5
 def poparea():
@@ -92,12 +89,12 @@ def title(buf):
 # @see https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
 # @since 20240102 copied to bbsengine6
 def updateprogress(iteration, total, fill="#"):
-  terminalwidth = ttyio.getterminalwidth()
+  terminalwidth = io.terminal.width()
   decimals = 0
   length = terminalwidth-20
   percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
   filledLength = length * iteration // total
   bar = fill * filledLength + '.' * (length - filledLength)
-  buf = "{lightgreen}Progress [% 3s%%]: [%s]{/fgcolor}" % (percent, bar)
+  buf = f"{{var:labelcolor}}Progress [{{var:valuecolor}}{percent:3s}%{{var:labelcolor}}]: [{bar}]{{/fgcolor}}"
   updatebottombar(buf)
   return
