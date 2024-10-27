1e3cbc22032cc19fb2366c476313ca91d44ec286
Jeff MacDonald <jam@zoidtechnologies.com>
2024-10-27 13:39:50 -0400

    - bbsengine6/con/main.py:   * renamed 'Quit' to 'Exit'   * added 'A' -- "Member Approval" option   * added call to session.start() and check it for errors and if so, return False   * from bbsengine6 import database

--- py/src/con/main.py
+++ py/src/con/main.py
@@ -1,4 +1,4 @@
-from bbsengine6 import session, util, io
+from bbsengine6 import session, util, io, database
 #import bbsengine6 as bbsengine
 
 from . import lib
@@ -16,17 +16,24 @@ def main(args, **kw):
   parser = lib.buildargs()
   args = parser.parse_args()
 
+  if session.start(args) is False:
+    io.echo("con.__main__.100: could not start session", level="error")
+    return False
+
+  lib.checkroles(args)
+
   done = False
   while not done:
     session.updatelastactivity(args, session.currentsessionid)
 
     util.heading("con")
-    io.echo("{f6}{var:labelcolor}database: {var:valuecolor}%s {var:labelcolor}host: {var:valuecolor}%s:%s{f6}" % (args.databasename, args.databasehost, args.databaseport))
+    io.echo(f"{{f6}}{{var:labelcolor}}database: {{var:valuecolor}}{args.databasename} {{var:labelcolor}}host: {{var:valuecolor}}{args.databasehost}{{var:labelcolor}}:{{var:valuecolor}}{args.databaseport}{{f6}}")
 
     io.echo("{var:optioncolor}[M]{var:labelcolor} Members")
     io.echo("{var:optioncolor}[S]{var:labelcolor} Sessions")
+#    io.echo("{var:optioncolor}[A]{var:labelcolor} Member Approval")
 #    ttyio.echo("[E]mail")
-    io.echo("{f6}{var:optioncolor}[X]{var:labelcolor} Quit{f6}")
+    io.echo("{f6}{var:optioncolor}[X]{var:labelcolor} Exit{f6}")
     ch = io.inputchoice("{var:promptcolor}console: {var:inputcolor}", "MSEXQ", "X")
     if ch == "M":
       io.echo("Members")
@@ -40,6 +47,9 @@ def main(args, **kw):
       io.echo("Sessions")
       lib.runmodule(args, "session")
       continue
+    elif ch == "A":
+      io.echo("Member Approval")
+      lib.runmodule(args, "memberapproval")
     elif ch == "Q" or ch == "X":
       io.echo("Exit")
       break
