e0dc94ba099707e08cbab6e33dcaaeefff6b40e0
Jeff MacDonald <jam@zoidtechnologies.com>
2024-10-27 10:04:24 -0400

    - bbsengine6/con/lib.py:   * fixed up lib.runmodule()   * changed call to setarea() to setbottombar()   * added checkroles()   * changed buildargs() so it calls database.buildargs()

--- py/src/con/lib.py
+++ py/src/con/lib.py
@@ -1,21 +1,21 @@
 import argparse
 
-import bbsengine6 as bbsengine
+from bbsengine6 import io, database, session, screen, module
 
 # @since 20230518 copied from teos
 def buildargs(args=None, **kw):
-    parser = argparse.ArgumentParser("teos")
+    parser = argparse.ArgumentParser("con")
     parser.add_argument("--verbose", action="store_true", dest="verbose")
     parser.add_argument("--debug", action="store_true", dest="debug")
 
     defaults = {"databasename": "zoid6", "databasehost":"localhost", "databaseuser": None, "databaseport":5432, "databasepassword":None}
-    bbsengine.database.buildargdatabasegroup(parser, defaults)
+    database.buildargs(parser, defaults)
     
     return parser
 
 # @since 20230523
-def runsubmodule(args, submodule, **kw):
-  return bbsengine.module.runmodule(args, f"con.{submodule}", **kw)
+def runmodule(args, submodule, **kw):
+  return module.runmodule(args, f"con.{submodule}", **kw)
 
 # @since 20230523 copied from teos
 def setarea(args, left, **kw):
@@ -24,5 +24,15 @@ def setarea(args, left, **kw):
         debug = " | debug" if args.debug is True else ""
         return f"con{debug}{help}"
 
-    bbsengine.screen.setarea(left, right)
+    screen.setbottombar(left, right)
     return
+
+def checkroles(args):
+  roles = ("bbs", "web", "sysop", "term")
+  for r in roles:
+    io.echo(f"checking for {r}: ", end="")
+    if database.rolexists(args, r) is False:
+      database.createrol(args, r)
+      io.echo("created")
+    else:
+      io.echo("exists")
