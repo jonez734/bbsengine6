a9215c90406e78d8b4a47c412a400b959cb13b0b
Jeff MacDonald <jam@zoidtechnologies.com>
2024-10-31 11:13:43 -0400

    - bbsengine6/sql/flagdata.sql: removed 'draft', 'frozen', and 'junk'; added 'approved' and 'emailverified'

--- sql/flagdata.sql
+++ sql/flagdata.sql
@@ -1,2 +1,13 @@
 \echo flagdata.sql
-insert into engine.flag(name, defaultvalue, description) values ('SYSOP', 'f', 'SysOp Access');
+insert into engine.flag(name, defaultvalue, description) values ('SYSOP',  'f', 'SysOp Access');
+insert into engine.flag(name, defaultvalue, description) values ('MAGIC',  'f', 'Magic Related');
+insert into engine.flag(name, defaultvalue, description) values ('EROS',   'f', 'Adult Content');
+insert into engine.flag(name, defaultvalue, description) values ('AUTHENTICATED', 'f', 'Authenticated Member');
+insert into engine.flag(name, defaultvalue, description) values ('ASIMOV', 'f', 'project asimov');
+insert into engine.flag(name, defaultvalue, description) values ('NOCALUMNI', 'f', 'NOC Alumni');
+insert into engine.flag(name, defaultvalue, description) values ('EMAILVERIFIED', 'f', 'E-Mail Verified');
+insert into engine.flag(name, defaultvalue, description) values ('APPROVED', 'f', 'Account Approved');
+
+--insert into engine.flag(name, defaultvalue, description) values ('DRAFT',  'f', 'Draft');
+--insert into engine.flag(name, defaultvalue, description) values ('FROZEN', 'f', 'Frozen');
+--insert into engine.flag(name, defaultvalue, description) values ('JUNK',   'f', 'Junk');
