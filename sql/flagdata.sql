fa2f0d4f1314022687d8e5d6366add551e710d8e
Jeff MacDonald <jam@zoidtechnologies.com>
2024-11-14 09:08:08 -0500

    - bbsengine6/sql/flagdata.sql: cosmetic changes (MAGIC and ASIMOV)

--- sql/flagdata.sql
+++ sql/flagdata.sql
@@ -1,9 +1,9 @@
 \echo flagdata.sql
 insert into engine.flag(name, defaultvalue, description) values ('SYSOP',  'f', 'SysOp Access');
-insert into engine.flag(name, defaultvalue, description) values ('MAGIC',  'f', 'Magic Related');
+insert into engine.flag(name, defaultvalue, description) values ('MAGIC',  'f', 'Magician');
 insert into engine.flag(name, defaultvalue, description) values ('EROS',   'f', 'Adult Content');
 insert into engine.flag(name, defaultvalue, description) values ('AUTHENTICATED', 'f', 'Authenticated Member');
-insert into engine.flag(name, defaultvalue, description) values ('ASIMOV', 'f', 'project asimov');
+insert into engine.flag(name, defaultvalue, description) values ('ASIMOV', 'f', 'Project Asimov');
 insert into engine.flag(name, defaultvalue, description) values ('NOCALUMNI', 'f', 'NOC Alumni');
 insert into engine.flag(name, defaultvalue, description) values ('EMAILVERIFIED', 'f', 'E-Mail Verified');
 insert into engine.flag(name, defaultvalue, description) values ('APPROVED', 'f', 'Account Approved');
