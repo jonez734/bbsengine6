34f680adae255e56d41be7e8c5ffe26a3084fc32
Jeff MacDonald <jam@zoidtechnologies.com>
2025-03-17 16:15:07 -0400

    - con/session.py: **kw -> **kwargs, minor tweaks

--- py/src/con/session.py
+++ py/src/con/session.py
@@ -5,25 +5,30 @@ from datetime import datetime
 
 from bbsengine6 import io, util, database, member
 
-def init(args, **kw):
+def init(args, **kwargs):
     return True
 
-def access(args, op, **kw):
+def access(args, op, **kwargs):
     return True
 
-def buildargs(args, **kw):
+def buildargs(args, **kwargs):
     return None
 
-def main(args, **kw):
+def main(args, **kwargs):
     util.heading("system sessions summary")
 
     time.tzset()
     # tz = datetime.tzinfo("US/Pacific") # .tzname # ("US/Pacific")
     localtz = dateutil.tz.tzlocal()
 
+    # conn = kwargs.get("conn", None)
+    pool = kwargs.get("pool", None)
+    if pool is None:
+        io.echo(f"bbsengine.con.session.main.100: {pool=}", level="error")
+        return False
+
     try:
-        with database.connect(args) as conn:
-            io.echo(f"connecton obtained {conn=}", level="debug")
+        with database.connect(args, **kwargs) as conn:
             with database.cursor(conn) as cur:
                 sql = "select * from engine.session order by datecreated"
                 cur.execute(sql)
@@ -32,14 +37,16 @@ def main(args, **kw):
                     return True
 
                 for session in database.resultiter(cur):
-#                    io.echo(f"{session=}", level="debug")
-                    m = member.getbymoniker(args, session["moniker"])
-                    if m is None:
-                        continue
+                    io.echo(f"bbsengine.con.session.100: {session['moniker']=}", level="debug")
+                    m = member.getbymoniker(args, session["moniker"], conn=conn, **kwargs)
+                    io.echo(f"bbsengine.con.session.120: {m=}", level="debug")
+
+#                    if m is None:
+#                        continue
                     la = util.timedelta(datetime.now(tz=localtz) - session["lastactivity"])
                     ex = util.timedelta(session["expiry"] - datetime.now(tz=localtz))
 
-                    io.echo(f"{{var:labelcolor}}Moniker:    {{var:valuecolor}}{m['moniker']}")
+                    io.echo(f"{{var:labelcolor}}Moniker:    {{var:valuecolor}}{session['moniker']}")
                     io.echo(f"{{var:labelcolor}}Created:    {{var:valuecolor}}{util.datestamp(session['datecreated'])}")
                     io.echo(f"{{var:labelcolor}}Expiry:     {{var:valuecolor}}{util.datestamp(session['expiry'])} {{var:labelcolor}}({{var:valuecolor}}{ex}{{var:labelcolor}})")
                     io.echo(f"{{var:labelcolor}}Actvity:    {{var:valuecolor}}{util.datestamp(session['lastactivity'])} {{var:labelcolor}}({{var:valuecolor}}{la}{{var:labelcolor}})")
