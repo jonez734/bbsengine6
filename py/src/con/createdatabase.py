0633acc14384c1d7d40a73202b83f2922abad71a
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-15 22:02:15 -0400

    - bbsengine6/con/createdatabase.py: added

---
+++ py/src/con/createdatabase.py
@@ -0,0 +1,23 @@
+from bbsengine6 import session, util, io, database
+#import bbsengine6 as bbsengine
+
+from . import lib
+
+def init(args, **kwargs):
+    return True
+
+def buildargs(args, **kwargs):
+    return lib.buildargs(args, **kwargs)
+
+def access(args, op, **kwargs):
+    return True
+
+def main(args, **kwargs):
+    parser = buildargs(args)
+    args = parser.parse_args()
+    io.echo(f"{{var:labelcolor}}creating database {{var:valuecolor}}{args.databasename}")
+    if database.create(args, args.databasename, **kwargs) is False:
+        io.echo(f"unable to create database {args.databasename}", level="error")
+        return False
+    io.echo(f"{{var:valuecolor}}{args.databasename}{{var:labelcolor}} created")
+    return True
