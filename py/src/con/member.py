f964b404efd9763a6ca6b5ff252522ce53cb37a7
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-15 21:50:04 -0400

    - bbsengine6/con/member.py:   * kw -> kwargs   * pass kwargs to database functions   * setui() now accepts kwargs   * configurerole() works now   * add and edit of accounts works now

--- py/src/con/member.py
+++ py/src/con/member.py
@@ -7,16 +7,16 @@ from bbsengine6 import member as libmember
 
 from . import lib
 
-def init(*args, **kw):
+def init(args, **kwargs):
   return True
 
-def buildargs(args, **kw):
-  return lib.buildargs(args, **kw)
+def buildargs(args, **kwargs):
+  return lib.buildargs(args, **kwargs)
 
-def access(args, op, **kw):
+def access(args, op, **kwargs):
   return True
 
-def editflags(args, moniker=None):
+def editflags(args, moniker=None, **kwargs):
     """Edits flags for a member.
 
     Args:
@@ -25,21 +25,24 @@ def editflags(args, moniker=None):
             If None, returns the flags with default values.
     """
 
-    flags = libmember.getflags(args, moniker)
+    conn = kwargs.get("conn", None)
+    flags = libmember.getflags(args, moniker, conn=conn)
+    io.echo(f"bbsengine.con.member.100: {flags=}", level="debug")
     updated_flags = {flag_name: {"value":flag_data["value"]} for flag_name, flag_data in flags.items()}
 
     for flag_name, flag_data in flags.items():
         description = flag_data["description"]
         current_value = flag_data["value"]
-        default_value = "Y" if util.toboolean(current_value) else "N"
-        io.echo(f"{default_value=} {current_value=}", level="debug")
+#        default_value = "Y" if util.tobool(current_value) else "N"
+        io.echo(f"{current_value=}", level="debug")
         prompt = f"{{var:promptcolor}}{description} "
-        if default_value == "Y":
+        if current_value is True:
           prompt += f"[{{var:currentoptioncolor}}Y{{var:optioncolor}}n{{var:promptcolor}}]"
         else:
           prompt += f"[{{var:optioncolor}}y{{var:currentoptioncolor}}N{{var:promptcolor}}]"
         prompt += f"{{promptcolor}}: {{var:inputcolor}}"
-        new_value = io.inputboolean(f"{prompt}", default=default_value)
+        default = "Y" if current_value is True else "N"
+        new_value = io.inputboolean(prompt, default=default)
         io.echo("{/all}", end="")
         if new_value != current_value:
             # TODO: Implement logic to update the flag in the database
@@ -72,11 +75,30 @@ def showui(args, ui, _ui):
     io.echo(f"  {{var:optioncolor}}[U]{{var:labelcolor}} UI: {', '.join(ui)}")
   return
 
-def help(*args, **kw):
-  io.echo(f"{kw=}", level="debug")
+def help(args, **kwargs):
+  member = kwargs.get("member", {})
+  _member = kwargs.get("_member", {})
+  conn = kwargs.get("conn", None)
 
-  member = kw["member"] if "member" in kw else {}
-  _member = kw["_member"] if "_member" in kw else {}
+  def _format_flags(flags):
+    """
+    Formats the flag names based on their values.
+    If the value is True, the name is wrapped with '**'.
+    If the value is False, the name is returned as is.
+
+    Args:
+        flags (dict): A dictionary of flags with 'description' and 'value' keys.
+
+    Returns:
+        str: A formatted string of flag names.
+    """
+    formatted_flags = []
+    for name, flag in flags.items():
+        if flag['value']:
+            formatted_flags.append(f":checkmark: {name}")
+        else:
+            formatted_flags.append(f":crossmark: {name}")
+    return " ".join(formatted_flags)
 
   if member["moniker"] is None:
     io.echo(f"{{red}} *{{var:optioncolor}}[M]{{var:labelcolor}} Moniker")
@@ -95,9 +117,9 @@ def help(*args, **kw):
   if member["email"] is None:
     io.echo(f"{{red}} *{{var:optioncolor}}[E]{{var:labelcolor}} Email")
   elif member["email"] != _member["email"]:
-    io.echo(f"{{red}} *{{var:optioncolor}}[E]{{var:labelcolor}}mail: {{var:valuecolor}}{member['email']} {{var:labelcolor}}(was: {{var:valuecolor}}{_member['email']})")
+    io.echo(f"{{red}} *{{var:optioncolor}}[E]{{var:labelcolor}} Email: {{var:valuecolor}}{member['email']} {{var:labelcolor}}(was: {{var:valuecolor}}{_member['email']})")
   else:
-    io.echo(f"{{red}} *{{var:optioncolor}}[E]{{var:labelcolor}}mail: {member['email']}")
+    io.echo(f"{{red}} *{{var:optioncolor}}[E]{{var:labelcolor}} Email: {member['email']}")
 
   if member["password"] is None:
     io.echo("  {var:optioncolor}[P]{var:labelcolor} Password{/all}")
@@ -108,13 +130,15 @@ def help(*args, **kw):
     io.echo(f"  {{var:optioncolor}}[C]{{var:labelcolor}} Credits: {{var:valuecolor}}{member['credits']} (was: {_member['credits']})")
   else:
     io.echo(f"  {{var:optioncolor}}[C]{{var:labelcolor}} Credits: {{var:valuecolor}}{member['credits']}")
-    
+
   showui(args, member["ui"], _member["ui"])
     
-#  flags = member["flags"] or {}
-  set_flags = [flag for flag, data in member["flags"].items() if data["value"] is True]
+  flags = member["flags"] or {}
+  #io.echo(f"bbsengine.con.member.help.100: {flags=} {member['flags']=}", level="debug")
+  #set_flags = [flag for flag, data in flags.items() if data["value"] is True]
+  set_flags = _format_flags(flags)
   if len(set_flags) > 0:
-    io.echo(f"  {{var:optioncolor}}[F]{{var:labelcolor}} Flags:{{var:valuecolor}} {', '.join(set_flags)}")
+    io.echo(f"  {{var:optioncolor}}[F]{{var:labelcolor}} Flags:{{var:valuecolor}} {set_flags}")
   else:
     io.echo(f"  {{var:optioncolor}}[F]{{var:labelcolor}} Flags{{var:valuecolor}}")
 
@@ -132,11 +156,13 @@ def help(*args, **kw):
   elif member["refcode"] != _member["refcode"]:
     io.echo(f"  {{var:optioncolor}}[R]{{var:labelcolor}} Refcode: {member['refcode']} {{var:labelcolor}}(was: {{var:valuecolor}}{_member['refcode']}{{var:labelcolor}})")
 
-def _edit(args, mode, member, **kw):
+def _edit(args, mode, member, **kwargs):
+#  cur = kwargs.get("cur", None)
   _member = copy.deepcopy(member)
+  io.echo(f"con.member._edit.100: {kwargs=}", level="debug")
 
   util.heading(f"{mode} member")
-
+  conn = kwargs.get("conn", None)
   done = False
   while not done:
     help(args, member=member, _member=_member)
@@ -147,13 +173,13 @@ def _edit(args, mode, member, **kw):
       _moniker = member["moniker"]
       moniker = io.inputstring("{var:promptcolor}moniker: {var:inputcolor}", _moniker, args=args) # verify=bbsengine.member.verifyMemberFound, args=args)
       if _moniker != moniker:
-        if libmember.verifyMemberFound(args, moniker, column="moniker") is True:
+        if libmember.verifyMemberFound(args, moniker, column="moniker", **kwargs) is True:
           io.echo(f"{moniker} is already in use.", level="error")
         else:
           member["moniker"] = moniker
     elif ch == "F":
       io.echo("Flags")
-      member["flags"] = editflags(args, member["moniker"])
+      member["flags"] = editflags(args, member["moniker"], conn=conn)
     elif ch == "L":
       io.echo("Loginid")
       member["loginid"] = io.inputstring("{var:promptcolor}loginid: {var:inputcolor}", member["loginid"], args=args)
@@ -170,9 +196,10 @@ def _edit(args, mode, member, **kw):
       io.echo("E-mail address")
       member["email"] = io.inputstring("{var:promptcolor}e-mail address: {var:inputcolor}")
       if member["email"] != _member["email"]:
-        member["emailverified"] = False
-        member["emailverifiedbymoniker"] = None
-        member["dateemailverified"] = None
+        io.echo("FIXME: set emailverified flag to false", level="info")
+#        member["emailverified"] = False
+#        member["emailverifiedbymoniker"] = None
+#        member["dateemailverified"] = None
     elif ch == "A":
       io.echo("Alerts")
       alerts(args)
@@ -215,166 +242,195 @@ def editui(args, rolename):
 #    database.manage_secondary_role(args, rolename, "remove", "term")
   return ui
 
-def setui(args, rolename, ui):
+def setui(args, rolname, ui, **kwargs):
+  io.echo(f"bbsengine.con.setui.100: {kwargs=}", level="debug")
   if ui is None:
     ui = []
   if "term" in ui:
-    database.manage_secondary_role(args, rolename, "add", "term")
+    database.manage_secondary_role(args, rolname, "grant", "term", **kwargs)
   else:
-    database.manage_secondary_role(args, rolename, "remove", "term")
+    database.manage_secondary_role(args, rolname, "revoke", "term", **kwargs)
 
   if "web" in ui:
-    database.manage_secondary_role(args, rolename, "add", "web")
+    database.manage_secondary_role(args, rolname, "grant", "web", **kwargs)
   else:
-    database.manage_secondary_role(args, rolename, "remove", "web")
+    database.manage_secondary_role(args, rolname, "revoke", "web", **kwargs)
 
-def configurerole(args, rolename):
-  io.echo(f"{{var:labelcolor}}checking for database role {{var:valuecolor}}{rolename}")
-  if database.rolexists(args, rolename) is False:
+def configurerole(args, rolename:str, sysop=False, **kwargs:dict) -> bool:
+  conn = kwargs.get("conn", None)
+  io.echo(f"con.member.configurerole.100: {conn=}", level="debug")
+  if conn is None:
+    return False
+
+  io.echo(f"{{var:labelcolor}}checking for role {{var:valuecolor}}{rolename}")
+  io.echo(f"con.member.configurerole.100: {kwargs=}", level="debug")
+  if database.rolexists(args, rolename, **kwargs) is False:
     if io.inputboolean(f"{{var:promptcolor}}role {{var:valuecolor}}{rolename}{{var:promptcolor}} does not exist. create it? {{var:optioncolor}}[Yn]{{var:promptcolor}}: {{var:inputcolor}}", "Y") is False:
       io.echo("role not created.")
       return False
-    database.createrol(args, rolename)
+    io.echo(f"bbsengine.con.configurerole.120: trying to create role", level="debug")
+    database.createrol(args, rolename, conn=conn, **kwargs)
 
-  database.manage_role_privs(args, rolename, "grant", "login")
-  database.manage_role_privs(args, rolename, "grant", "inherit")
+    io.echo(f"bbsengine.con.configurerole.140: calling manage_role_privs()", level="debug")
+    database.manage_role_privs(args, rolename, "grant", "login", conn=conn, **kwargs)
+    database.manage_role_privs(args, rolename, "grant", "inherit", conn=conn,  **kwargs)
+
+  if sysop is True:
+    database.manage_secondary_role(args, rolename, "grant", "sysop", **kwargs)
+    database.manage_role_privs(args, rolename, "grant", "createdb", **kwargs)
+    # database.manage_role_privs(args, loginid, "grant", "superuser", **kwargs)
+    database.manage_role_privs(args, rolename, "grant", "createrole", **kwargs)
+  else:
+    database.manage_secondary_role(args, rolename, "revoke", "sysop", **kwargs)
+    database.manage_role_privs(args, rolename, "revoke", "createdb", **kwargs)
+    # database.manage_role_privs(args, loginid, "revoke", "superuser")
+    database.manage_role_privs(args, rolename, "revoke", "createrole", **kwargs)
+
+  io.echo(f"con.member.configurerole.160: done", level="debug")
   return True
 
-def edit(args, **kw):
+def edit(args, **kwargs):
+  io.echo(f"bbsengine.con.member.100: {kwargs=}", level="debug")
   buf = io.inputstring("{var:promptcolor}loginid or moniker: {var:inputcolor}", "", noneok=True)
   io.echo("{/all}")
   if buf is None:
     return None
-  sql = "select * from engine.__member where loginid=%s or moniker=%s"
-  dat = (buf, buf)
   
-  with database.connect(args) as conn:
-    with database.cursor(conn) as cur:
+  pool = kwargs.get("pool", None)
+  if pool is None:
+    io.echo("bbsengine.con.member.200: {pool=}", level="error")
+    return None
+
+  with database.connect(args, **kwargs) as conn:
+    sql = "select * from engine.__member where loginid=%s or moniker=%s"
+    dat = (buf, buf)
+
+    with database.cursor(conn, **kwargs) as cur:
       cur.execute(sql, dat)
       if cur.rowcount == 0:
-        io.echo(f"{buf!r} not found", level="error")
+        io.echo(f"{buf=} not found", level="error")
         return False
 
       rec = cur.fetchone()
 
-      m = libmember.build(args, rec)
-      m["flags"] = libmember.getflags(args, m["moniker"])
+      m = libmember.build(args, rec, conn=conn, **kwargs)
 
       io.echo(f"bbsengine6.con.member.edit.120: {rec=} {m=}", level="debug")
 
-      _m = _edit(args, "edit", m)
+      _m = _edit(args, "edit", m, conn=conn)
 
       loginid = m["loginid"]
       moniker = m["moniker"]
       memberid = m["id"]
       password = m["password"]
       ui = m["ui"]
-#      flags = libmember.getflags(args, moniker)
-#      sysop = flags["SYSOP"]["value"]
 
-      configurerole(args, loginid)
+      configurerole(args, loginid, conn=conn, **kwargs)
 
-      if io.inputboolean(f"{{var:promptcolor}}save changes? {{var:optioncolor}}[Yn]{{var:promptcolor}}: {{var:inputcolor}}", "Y"):
-        io.echo("Yes")
-        setui(args, loginid, ui)
+      io.echo(f"con.member.edit.100: calling setui()", level="debug")
+      setui(args, loginid, ui, conn=conn, **kwargs)
 
-        if _m["email"] != m["email"]:
-          libmember.setflag(args, "EMAILVERIFIED", False, moniker=m["moniker"], mogrify=True)
-          m["emailverifiedbymoniker"] = None
-          m["dateemailverified"] = None
+      if _m["email"] != m["email"]:
+        libmember.setflag(args, "EMAILVERIFIED", False, moniker=m["moniker"], mogrify=True)
 
-        libmember.update(args, m, memberid)
-        libmember.setpassword(args, moniker, password)
+      libmember.update(args, m, moniker, conn=conn, **kwargs)
+      libmember.setpassword(args, moniker, password, conn=conn, **kwargs)
 
-        conn.commit() # database.commmit(args)
-      else:
-        io.echo("changes not saved.")
-        conn.rollback()
+  if io.inputboolean(f"{{var:promptcolor}}save changes? {{var:optioncolor}}[Yn]{{var:promptcolor}}: {{var:inputcolor}}", "Y"):
+    io.echo("commiting member.")
+    conn.commit()
+  else:
+    io.echo("changes not saved.")
+    conn.rollback()
 
-      return True
-
-
-def add(args):
-  _member = libmember.build(args)
-
-  io.echo(f"con.member.add.160: {_member=}", level="debug")
-  member = _edit(args, "add", _member)
-  io.echo(f"con.member.add.140: {member=}", level="debug")
-  if member is None:
-    io.echo(f"con.member.add.180: _edit returned None", level="error")
-    return False
-
-  if io.inputboolean("{var:promptcolor}add member? {var:optioncolor}[yN]{var:promptcolor}: {var:inputcolor}", "N") is False:
-    io.echo("{/all}member not added.")
     return True
 
-  with database.connect(args) as conn:
+def add(args, **kwargs) -> bool:
+  io.echo(f"bbsengine.con.member.add.220: {kwargs=}", level="debug")
+  pool = kwargs.get("pool", None)
+  io.echo(f"bbsengine.con.member.200: {pool=}", level="debug")
+  if pool is None:
+    return False
+
+  with database.connect(args, pool=pool) as conn:
+    _member = libmember.build(args, conn=conn, **kwargs)
+
+    member = _edit(args, "add", _member, conn=conn, **kwargs)
+    io.echo(f"con.member.add.140: {member=}", level="debug")
+    if member is None:
+      io.echo(f"con.member.add.180: _edit returned None", level="error")
+      return False
+
+    # pool = kwargs.get("pool", None)
     member["datecreated"] = "now()"
-    member["createdbymoniker"] = libmember.getcurrentmoniker(args)
+    member["createdbymoniker"] = libmember.getcurrentmoniker(args, conn=conn, **kwargs)
     io.echo(f"con.member.add.100: {member=}", level="debug")
 
     if args.debug is True:
       io.echo(f"con.member.add.120: {member=}", level="debug")
 
-    moniker = libmember.insert(args, member, primarykey="moniker") # =member, mogrify=True, returnid=True, primarykey="moniker")
+    moniker = libmember.insert(args, member, primarykey="moniker", conn=conn, **kwargs) # =member, mogrify=True, returnid=True, primarykey="moniker")
+    if args.debug is True:
+      io.echo(f"{moniker=}", level="debug")
 
     for name, data in member["flags"].items():
       io.echo(f"{name=} {data=}", level="debug")
-      libmember.setflag(args, name, data["value"], moniker=moniker)
+      libmember.setflag(args, name, data["value"], moniker=moniker, conn=conn, **kwargs)
 
-    setui(args, member["loginid"], member["ui"])
+#    setui(args, member["loginid"], member["ui"], conn=conn, **kwargs)
+
   #  for name, data in flags.items():
   #    io.echo(f"{name=} {data=}", level="debug")
   #    libmember.setflag(args, name, data["value"], membermoniker)
 
-    if args.debug is True:
-      io.echo(f"{moniker=}", level="debug")
 
-    libmember.setattrs(args, {"foo":42}, moniker, reset=True)
-    libmember.setpassword(args, member["password"], moniker)
-    libmember.setcredits(args, member["credits"], moniker)
+#    libmember.setattrs(args, {}, moniker, reset=True, conn=conn, **kwargs)
+    if member["password"] is not None and member["password"] != "":
+      libmember.setpassword(args, member["password"], moniker, conn=conn, **kwargs)
+    libmember.setcredits(args, member["credits"], moniker, conn=conn, **kwargs)
 
-    loginid = member["loginid"]
-    configurerole(args, loginid)
-    if member["flags"]["SYSOP"]["value"] is True:
-      database.manage_secondary_role(args, loginid, "add", "sysop")
-      database.manage_role_privs(args, loginid, "grant", "createdb")
-      database.manage_role_privs(args, loginid, "grant", "superuser")
-      database.manage_role_privs(args, loginid, "grant", "createrole")
-    else:
-      database.manage_secondary_role(args, loginid, "remove", "sysop")
-      database.manage_role_privs(args, loginid, "revoke", "createdb")
-      database.manage_role_privs(args, loginid, "revoke", "superuser")
-      database.manage_role_privs(args, loginid, "revoke", "createrole")
+    configurerole(args, member["loginid"], member["flags"]["SYSOP"]["value"], conn=conn, **kwargs)
+
+    if io.inputboolean(f"{{var:promptcolor}}add member? {{var:optioncolor}}[Yn]: {{var:inputcolor}}", "Y") is False:
+      conn.rollback()
+      return False
 
     conn.commit()
-
-#  database.commit(args)
-  io.echo("{/all}member added.")
-
+    io.echo("{/all}member added.")
   return True
 
-def main(args, **kw):
-    parser = lib.buildargs(args, **kw)
+def main(args, **kwargs):
+    parser = lib.buildargs(args, **kwargs)
     args = parser.parse_args()
 
+    pool = kwargs.get("pool", None)
+    if pool is None:
+      io.echo(f"bbsengine.con.member.main.120: {pool=}", level="error")
+      return False
+#    conn = kwargs.get("conn", None)
+#    io.echo(f"bbsengine.con.member.100: {conn=}", level="debug")
+    
     done = False
     while not done:
         util.heading("member")
         io.echo(f"{{f6}}{{var:labelcolor}}database: {{var:valuecolor}}{args.databasename} {{var:labelcolor}}host: {{var:valuecolor}}{args.databasehost}:{args.databaseport}{{f6}}")
         io.echo(f"{{var:optioncolor}}[E]{{var:labelcolor}} Edit")
-        io.echo(f"{{var:optioncolor}}[N]{{var:labelcolor}} New user")
+        io.echo(f"{{var:optioncolor}}[N]{{var:labelcolor}} New Member")
         io.echo(f"{{var:optioncolor}}[A]{{var:labelcolor}} Approvals")
         io.echo(f"{{f6}}{{var:optioncolor}}[Q]{{var:labelcolor}} Quit")
-        ch = io.inputchoice(f"{{var:promptcolor}}member:{{var:inputcolor}} ", "NAEQ", "Q")
+        ch = io.inputchoice(f"{{var:promptcolor}}member:{{var:inputcolor}} ", "NAEQX", "Q")
         if ch == "E":
           io.echo("Edit")
-          edit(args)
-          continue
+          if edit(args, **kwargs) is False:
+            io.echo(f"edit aborted", level="error")
+          else:
+            io.echo("edit ok", level="ok")
         elif ch == "N":
-          io.echo("New User")
-          if add(args) is False:
-            io.echo(f"con.member.main.100: add failed", level="error")
+          io.echo("New")
+          if add(args, **kwargs) is False:
+            io.echo(f"add aborted", level="error")
+          else:
+            io.echo(f"add ok", level="ok")
         elif ch == "Q" or ch == "X":
           io.echo("Quit")
           done = True
