54a5e6d6b9beab3fac549620706e471076551e31
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-17 16:36:22 -0400

    - bbsengine6/sql/subscribe.sql: s/memberid/membermoniker/

--- sql/subscribe.sql
+++ sql/subscribe.sql
@@ -1,13 +1,13 @@
 create table if not exists engine.subscribe_blurb (
-    memberid bigint constraint fk_subscribe_blurb_memberid references engine.__member(id) on update cascade on delete cascade,
+    membermoniker text constraint fk_subscribe_blurb_memberid references engine.__member(moniker) on update cascade on delete cascade,
     blurbid bigint constraint fk_subscribe_blurb_blurbid references engine.__blurb(id) on update cascade on delete cascade
 );
 
-create unique index idx_subscribe_blurb on engine.subscribe_blurb(memberid, blurbid);
+create unique index idx_subscribe_blurb on engine.subscribe_blurb(membermoniker, blurbid);
 
 create table if not exists engine.subscribe_sig (
-    memberid bigint constraint fk_subscribe_sig_memberid references engine.__member(id) on update cascade on delete cascade,
+    membermoniker text constraint fk_subscribe_sig_memberid references engine.__member(moniker) on update cascade on delete cascade,
     sigpath ltree constraint fk_subscribe_sig_sigpath references engine.__sig(path) on update cascade on delete cascade
 );
 
-create unique index idx_subscribe_sig on engine.subscribe_sig(memberid, sigpath);
+create unique index idx_subscribe_sig on engine.subscribe_sig(membermoniker, sigpath);
