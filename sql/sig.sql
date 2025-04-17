8bf67e9c23bed4c654027eb95176379501b65f41
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-17 15:15:35 -0400

    - bbsengine6/sql/sig.sql: updatedby, approvedby, createdby are now citext instead of bigint

--- sql/sig.sql
+++ sql/sig.sql
@@ -1,30 +1,20 @@
-\echo sig
+--\echo sig
 create table engine.__sig (
     "path" ltree unique not null primary key,
     "uri"  text unique,
     "title" text,
     "intro" text,
-    "attributes" jsonb,
+    "attrs" jsonb,
+    "access" jsonb,
     "dateupdated" timestamptz,
-    "updatedbyid" bigint constraint fk_engine_sig_updatedbyid references engine.__member(id) on update cascade on delete set null,
+    "updatedbymoniker" citext constraint fk_engine_sig_updatedbymoniker references engine.__member(moniker) on update cascade on delete set null,
     "dateapproved" timestamptz,
-    "approvedbyid" bigint constraint fk_engine_sig_approvedbyid references engine.__member(id) on update cascade on delete set null,
+    "approvedbymoniker" citext constraint fk_engine_sig_approvedbymoniker references engine.__member(moniker) on update cascade on delete set null,
     "datecreated" timestamptz,
-    "createdbyid" bigint constraint fk_engine_sig_createdbyid references engine.__member(id) on update cascade on delete set null
+    "createdbymoniker" citext constraint fk_engine_sig_createdbymoniker references engine.__member(moniker) on update cascade on delete set null
 );
 
-create view engine.sig as
- select 
-  s.*,
-  coalesce(m1.name, 'a. nonymous'::text) as createdbyname,
-  coalesce(m2.name, 'a. nonymous'::text) as updatedbyname
- from engine.__sig as s
- left join engine.__member as m1 ON (m1.id = s.createdbyid)
- left join engine.__member as m2 ON (m2.id = s.updatedbyid)
-;
+CREATE INDEX idx_sig_attrs ON engine.__sig USING gin (attrs);
 
---create unique index idx_engine_sig_path on engine.__sig(path);
-
-grant select on engine.sig to :web;
-
-grant all on table engine.__sig to :web;
+grant select on table engine.__sig to web;
+grant all on table engine.__sig to sysop, term;
