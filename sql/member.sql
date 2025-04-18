17b501a13ae04fb10c7e83c5201c89c3bf1385aa
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-18 15:41:18 -0400

    - bbsengine6/sql/member.sql: memberid->membermoniker, added ui, tz, attrs, and refcode

--- sql/member.sql
+++ sql/member.sql
@@ -1,24 +1,34 @@
-\echo member.sql
+--\echo member.sql
 
 create table engine.__member (
-  "id" bigserial unique not null primary key,
-  "moniker" text unique not null,
+--  "id" bigserial unique not null primary key,
+  "moniker" citext unique not null,
   "email" text not null,
   "password" text,
   "credits" numeric(10,0),
-  "parentid" bigint constraint fk_member_parentid references engine.__member(id) on update cascade on delete set null,
+  "parentmoniker" citext constraint fk_member_parentid references engine.__member(moniker) on update cascade on delete set null,
   "datecreated" timestamptz,
-  "createdbyid" bigint constraint fk_member_createdbyid references engine.__member(id) on update cascade on delete set null,
+  "createdbymoniker" citext constraint fk_member_createdbyid references engine.__member(moniker) on update cascade on delete set null,
   "dateupdated" timestamptz,
-  "updatedbyid" bigint constraint fk_member_updatedbyid references engine.__member(id) on update cascade on delete set null,
-  "approvedbyid" bigint constraint fk_member_approvedbyid references engine.__member(id) on update cascade on delete set null,
+  "updatedbymoniker" citext constraint fk_member_updatedbyid references engine.__member(moniker) on update cascade on delete set null,
+  "approvedbymoniker" citext constraint fk_member_approvedbyid references engine.__member(moniker) on update cascade on delete set null,
   "dateapproved" timestamptz,
+--  "emailverified" boolean,
+--  "emailverifiedbymoniker" text constraint fk_member_emailverifiedbyid references engine.__member(moniker) on update cascade on delete set null,
+--  "dateemailverified" timestamptz,
+--  "verifiedbymoniker" text constraint fk_member_verifiedbyid references engine.__member(moniker) on update cascade on delete set null,
+--  "dateverified" timestamptz,
   "lastlogin" timestamptz,
   "lastloginfrom" inet,
   "loginid" text,
-  "ui" text,
-  "attributes" jsonb
+  "ui" citext,
+  "tz" citext,
+  "attrs" jsonb,
+  "refcode" citext
 );
 
-grant all on engine.__member to :web;
-grant all on engine.__member_id_seq to :web;
+grant select, update on engine.__member to web, term;
+grant all on engine.__member to sysop;
+
+--grant usage, select on engine.__member_id_seq to web, term;
+--grant all on engine.__member_id_seq to sysop;
