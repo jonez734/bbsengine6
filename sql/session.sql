de773fb1db51238017920b5da504fbdcaf7bd5ac
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-18 15:09:42 -0400

    - bbsengine6/sql/session.sql: added lastactivitylocal and expirylocal and memberid->membermoniker

--- sql/session.sql
+++ sql/session.sql
@@ -1,4 +1,4 @@
-\echo session.sql
+--\echo session.sql
 --
 -- Name: session; Type: TABLE; Schema: public; Owner: jam; Tablespace: 
 --
@@ -12,10 +12,10 @@ CREATE TABLE engine.__session (
     useragent text,
     datecreated timestamptz,
     dateupdated timestamptz,
-    memberid bigint constraint fk_session_memberid references engine.__member(id) on update cascade on delete cascade
+    moniker citext constraint fk_session_moniker references engine.__member(moniker) on update cascade on delete cascade
 );
 
-
+grant select, insert, update, delete on engine.__session to web, term, sysop;
 
 --alter table engine.__session
 --add constraint "fk_session_memberid"
@@ -26,11 +26,14 @@ create view engine.session as
     select
         s.*,
         extract(epoch from s.expiry) as expiryepoch,
-        extract(epoch from s.lastactivity) as lastactivityepoch
+        extract(epoch from s.lastactivity) as lastactivityepoch,
+        timezone(currentmember.tz, lastactivity) as lastactivitylocal,
+        timezone(currentmember.tz, expiry) as expirylocal
+
     from engine.__session as s
+    left outer join engine.__member as currentmember on (currentmember.loginid = CURRENT_USER)
 ;
 
 create unique index idx_session_sessionid_unique on engine.__session(id);
 
-grant select on engine.session to "www-data";
-grant insert, update, delete on engine.__session to "www-data";
+grant select on engine.session to web, term, sysop;
