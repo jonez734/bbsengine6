7e312d88d738054a601fce055b6b17292c1bf79e
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-19 19:48:15 -0400

    - bbsengine6/sql/map_member_flag.sql: added

---
+++ sql/map_member_flag.sql
@@ -0,0 +1,8 @@
+create table engine.map_member_flag (
+  "moniker" citext constraint fk_mmf_membermoniker references engine.__member(moniker) on update cascade on delete cascade,
+  "name" citext not null constraint fk_mmf_name references engine.flag(name) on update cascade on delete cascade,
+  "value" boolean
+);
+
+grant select on engine.map_member_flag to web, term;
+grant all on engine.map_member_flag to sysop;
