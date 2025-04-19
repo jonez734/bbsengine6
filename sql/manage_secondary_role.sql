b40ca866c116a1c4df55a98670a102e9d9e3a339
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-19 15:56:30 -0400

    - bbsengine6/sql/manage_secondary_role.sql: add -> grant, remove -> revoke, add execute permission to sysop

--- sql/manage_secondary_role.sql
+++ sql/manage_secondary_role.sql
@@ -1,19 +1,19 @@
-CREATE OR REPLACE FUNCTION engine.manage_secondary_role(
+CREATE OR REPLACE FUNCTION public.manage_secondary_role(
     role_name TEXT,
     action TEXT,
     secondary_role TEXT
 ) RETURNS VOID AS
 $$
 BEGIN
-    IF action = 'add' THEN
+    IF action = 'grant' THEN
         EXECUTE format('GRANT %I TO %I', secondary_role, role_name);
-    ELSIF action = 'remove' THEN
+    ELSIF action = 'revoke' THEN
         EXECUTE format('REVOKE %I FROM %I', secondary_role, role_name);
     ELSE
-        RAISE EXCEPTION 'Invalid action. Must be "add" or "remove".';
+        RAISE EXCEPTION 'Invalid action. Must be "grant" or "revoke".';
     END IF;
 END;
 $$
 LANGUAGE plpgsql;
 
-grant execute on function engine.manage_secondary_role to sysop;
+grant execute on function public.manage_secondary_role to sysop;
