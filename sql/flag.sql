b60be753f1bfb41ec89865d87ffa61d3d489b097
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-19 15:58:07 -0400

    - bbsengine6/sql/flag.sql: text -> citext; commented out engine.map_blurb_flag; permissions

--- sql/flag.sql
+++ sql/flag.sql
@@ -1,15 +1,19 @@
-\echo flag.sql
+--\echo flag
 create table engine.flag (
-  "name" text unique not null primary key,
+  "name" citext unique not null primary key,
   "description" text,
   "defaultvalue" boolean
 );
 
-create table engine.map_member_flag (
-  "memberid" bigint constraint fk_mmf_memberid references engine.__member(id) on update cascade on delete cascade,
-  "name" text not null constraint fk_mmf_name references engine.flag(name) on update cascade on delete cascade,
-  "value" boolean
-);
+grant select on engine.flag to web, term;
+grant all on engine.flag to sysop;
+
+--create table engine.map_blurb_flag (
+--  "blurbid" bigint constraint fk_mbf_blurbid references engine.__blurb(id) on update cascade on delete cascade,
+--  "name" text not null constraint fk_mbf_name references engine.flag(name) on update cascade on delete cascade,
+--  "value" text
+--);
 
 ---grant all on engine.flag, engine.map_member_flag to apache;
-grant all on engine.flag, engine.map_member_flag to :web;
+--grant select on engine.flag, engine.map_member_flag, engine.map_blurb_flag to :web;
+--grant all on engine.flag, engine.map_member_flag, engine.map_blurb_flag to :bbs;
