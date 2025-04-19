44ad7fa422f518f4df434f2c755324855cf81169
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-19 19:45:14 -0400

    - bbsengine6/sql/blurbview.sql: added index, left joins renamed to be clearer; untested

--- sql/blurbview.sql
+++ sql/blurbview.sql
@@ -1,3 +1,4 @@
+\echo view engine.blurb
 create or replace view engine.blurb as
     select
         b.*,
@@ -5,17 +6,17 @@ create or replace view engine.blurb as
         extract(epoch from b.datecreated) as datecreatedepoch,
         extract(epoch from b.dateupdated) as dateupdatedepoch,
         extract(epoch from b.dateapproved) as dateapprovedepoch,
-        coalesce(m1.name, 'a. nonymous'::text) as createdbyname,
-        coalesce(m2.name, 'a. nonymous'::text) as updatedbyname,
-        coalesce(m3.name, 'a. nonymous'::text) as approvedbyname,
+--        coalesce(createdby.moniker,  'a. nonymous'::text) as createdbymoniker,
+--        coalesce(updatedby.moniker,  'a. nonymous'::text) as updatedbymoniker,
+--        coalesce(approvedby.moniker, 'a. nonymous'::text) as approvedbymoniker,
         array(select distinct map.sigpath from engine.map_blurb_sig as map where map.blurbid = b.id order by map.sigpath) AS sigs,
         array(select distinct map.tag from engine.map_blurb_tag as map where map.blurbid = b.id order by map.tag) as tags,
         (select count(id) from (select id from engine.__blurb as subblurb where subblurb.parentid=b.id) as subblurbs) as subblurbcount
     from engine.__blurb as b
-    left join engine.__member as m1 ON (m1.id = b.createdbyid)
-    left join engine.__member as m2 ON (m2.id = b.updatedbyid)
-    left join engine.__member as m3 ON (m3.id = b.approvedbyid)
+    left join engine.__member as createdby ON (createdby.moniker = b.createdbymoniker)
+    left join engine.__member as updatedby ON (updatedby.moniker = b.updatedbymoniker)
+    left join engine.__member as approvedby ON (approvedby.moniker = b.approvedbymoniker)
 ;
 
 \echo grant engine.blurb
-grant select on engine.blurb to :web;
+grant select on engine.blurb to web;
