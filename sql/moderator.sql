f4dc4ee4705b28675b3beeaebb84a6ac73281324
Jeff MacDonald <jam@zoidtechnologies.com>
2025-04-19 17:41:07 -0400

    - bbsengine6/sql/moderator.sql: text -> citext

--- sql/moderator.sql
+++ sql/moderator.sql
@@ -1,11 +1,11 @@
 \echo map_sig_moderator
 create or replace table engine.map_sig_moderator (
     sigpath ltree constraint fk_map_sig_moderator_sigpath references engine.sig(path) on update cascade on delete cascade,
-    membermoniker text constraint fk_map_sig_moderator_membermoniker references engine.__member(moniker) on update cascade on delete cascade,
+    membermoniker citext constraint fk_map_sig_moderator_membermoniker references engine.__member(moniker) on update cascade on delete cascade,
     datecreated timestamptz,
-    createdbymoniker text constraint fk_map_sig_moderator_createdbymoniker references engine.__member(moniker) on update cascade on delete set null,
+    createdbymoniker citext constraint fk_map_sig_moderator_createdbymoniker references engine.__member(moniker) on update cascade on delete set null,
     dateapproved timestamptz,
-    approvedbymoniker text constraint fk_map_sig_moderator_approvedbymoniker references engine.__member(moniker) on update cascade on delete set null
+    approvedbymoniker citext constraint fk_map_sig_moderator_approvedbymoniker references engine.__member(moniker) on update cascade on delete set null
 );
 
 create unique index if not exists idx_map_sig_moderator on engine.map_sig_moderator (membermoniker, sigpath);
