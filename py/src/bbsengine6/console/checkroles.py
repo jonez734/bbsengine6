f96629fbd3f4ce38b3566d37fb4fab4b5e99ad0f
Jeff MacDonald <jam@zoidtechnologies.com>
2025-05-10 17:55:11 -0400

    - bbsengine6/console/checkroles.py: fixed indentation mistake that only created one role; changed buildargs() to return None

--- py/src/bbsengine6/console/checkroles.py
+++ py/src/bbsengine6/console/checkroles.py
@@ -1,11 +1,10 @@
-from bbsengine6 import database, io, util
-from . import lib
+from bbsengine6 import database, io
 
 def init(args, **kwargs):
     return True
 
 def buildargs(args, **kwargs):
-    return lib.buildargs(args, **kwargs)
+    return None
 
 def access(args, op, **kwargs):
     return True
@@ -25,21 +24,4 @@ def main(args, **kwargs):
     else:
       io.echo("ok", level="ok")
 
-  io.echo(f"{{var:labelcolor}}role {{var:valuecolor}}www-data{{var:valuecolor}}: ", end="")
-  if database.rolexists(args, "www-data", **kwargs) is False:
-    io.echo(f"{{var:labelcolor}}create ")
-    if database.createrol(args, "www-data", login=True) is False:
-      io.echo(f"{{var:labelcolor}}error")
-      return False
-    else:
-      io.echo(f"ok")
-  else:
-    io.echo("ok")
-  
-  io.echo(f"{{var:labelcolor}}granting {{var:valuecolor}}login{{var:labelcolor}} to role {{var:valuecolor}}www-data{{var:labelcolor}}: ", end="")
-  if database.manage_role_privs(args, "www-data", "grant", "login", **kwargs) is False:
-#    io.echo(f"{{var:labelcolor}}failed")
-    return False
-  else:
-    io.echo("ok")
-    return True
+  return True
