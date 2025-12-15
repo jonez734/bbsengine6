36815116c89fc172b2183ceb2ab40b74becd22b8
Jeff MacDonald <jam@zoidtechnologies.com>
2025-12-14 19:20:33 -0500

    - bbsengine6/io/const.py: added OSC (terminal title, amongst other functions), MAX_TERMINAL_WIDTH, and FALLBACK_TERMINAL_WIDTH

--- py/src/bbsengine6/io/const.py
+++ py/src/bbsengine6/io/const.py
@@ -1,10 +1,13 @@
 ESC   = "\x1b" # \x9b
 CSI   = f"{ESC}["
+OSC   = f"{ESC}]"
 BEL  = "\007"
 ETX = '\x03' # ctrl-c
 EOF = '\x04' # ctrl-d
 
-MAX_TERMINAL_WIDTH = None # 100
+MAX_TERMINAL_WIDTH = None # actual terminal width
+FALLBACK_TERMINAL_WIDTH = 100
+
 DEFAULT_PALETTE_NAME = "c64"
 
 ECHO_END = "\n"
