c9280ea8aa4c8c63f0f3720e64f2adf75183fe30
J (eff) <jam@zoidtechnologies.com>
2026-03-15 10:52:39 -0400

    Add getdate module for parsing date expressions

---
+++ py/src/bbsengine6/getdate.py
@@ -0,0 +1,54 @@
+import time
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+
+#import datetime
+import dateutil.tz
+
+add_default_tz = lambda x, tzinfo: x.replace(tzinfo=x.tzinfo or tzinfo)
+
+#@since 20231203 merged from getdate3
+def getdate(buf):
+    time.tzset()
+#    tz = datetime.tzinfo("US/Pacific") # .tzname # ("US/Pacific")
+    localtz = dateutil.tz.tzlocal()
+
+    buf = buf.strip()
+    if buf == "now":
+        return datetime.now(tz=localtz)
+    elif buf == "tomorrow":
+        return datetime.now(tz=localtz) + timedelta(days=+1)
+    elif buf == "yesterday":
+        return datetime.now(tz=localtz) + timedelta(days=-1)
+    elif buf == "+2 days" or buf == "2 days":
+        return datetime.now(tz=localtz) + timedelta(days=+2)
+    elif buf == "-2 days":
+        return datetime.now(tz=localtz) + timedelta(days=-2)
+    elif buf == "today":
+      res = datetime.now(tz=localtz)
+      io.echo(f"{res=}", level="debug")
+      return datetime.now(tz=localtz)
+    elif buf == "last week":
+      return datetime.now(tz=localtz) + timedelta(days=-7)
+    elif buf == "next week":
+      return datetime.now(tz=localtz) + timedelta(days=+7)
+    else:
+      try:
+        res = add_default_tz(parse(buf), localtz)
+      except dateutil.parser._parser.ParserError:
+        return None
+      else:
+        return res
+
+def verifyValidDateExpression(buf, **kwargs):
+    if getdate(buf) is not None:
+        return True
+    return False
+
+def date(args, prompt, value, **kw):
+    buf = io.inputstring(prompt, value, verify=verifyValidDateExpression)
+#    return buf
+    res = getdate(buf)
+    if res is None:
+        ttyio.echo("invalid date expression")
+    return res
