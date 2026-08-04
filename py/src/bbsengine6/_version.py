1457738558a07e14c932c4c5612375a637f0b187
J (eff) <jam@zoidtechnologies.com>
2026-08-03 20:42:02 -0400

    bbsengine6: make install runs version and rebuilds with --no-cache-dir

--- py/src/bbsengine6/_version.py
+++ py/src/bbsengine6/_version.py
@@ -1,3 +1,2 @@
-__version__ = "0.0.1.dev202607221905"
-__datestamp__ = "202607221905"
-__githash__ = "e2b6e38ddae52faa"
+__version__ = "0.0.1.dev202608032039"
+__datestamp__ = "20260803-2039-opencode"
