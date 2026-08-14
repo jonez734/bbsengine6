b8955dc1bd1fc68c876e413faf0f9633910fd257
J (eff) <jam@zoidtechnologies.com>
2026-08-14 15:36:22 -0400

    bbsengine6: bump version for submodule pointer at d5856ad

--- py/src/bbsengine6/_version.py
+++ py/src/bbsengine6/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608140857"
-__datestamp__ = "202608140857"
-__githash__ = "9c29fc00ef672f6c"
+__version__ = "0.0.1.dev202608141400"
+__datestamp__ = "202608141400"
+__githash__ = "d5856ad4f0550201"
