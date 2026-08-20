b4e129b5c5b75e42d7608611be5893c59ca36ec9
J (eff) <jam@zoidtechnologies.com>
2026-08-20 14:31:48 -0400

    chore(bbsengine6): bump _version.py to 202608201431

--- py/src/bbsengine6/_version.py
+++ py/src/bbsengine6/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608200852"
-__datestamp__ = "202608200852"
-__githash__ = "b3294d5b8ab1a685"
+__version__ = "0.0.1.dev202608201431"
+__datestamp__ = "202608201431"
+__githash__ = "587ce1089a4fc375"
