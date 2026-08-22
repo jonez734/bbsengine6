edffb00f5425e0b0d23981093e6e8826781db1f5
J (eff) <jam@zoidtechnologies.com>
2026-08-22 13:28:13 -0400

    chore(bbsengine6): bump _version.py to 202608221617
    Auto-regenerated by 'make version' on 2026-08-22 16:17 UTC.
    Bundled with the docs commit but split into its own so the
    version-only bump is greppable for tooling that filters on
    the chore/* prefix.

--- py/src/bbsengine6/_version.py
+++ py/src/bbsengine6/_version.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.1.dev202608211924"
-__datestamp__ = "202608211924"
-__githash__ = "b5ffb3544edc5d7d"
+__version__ = "0.0.1.dev202608221229"
+__datestamp__ = "202608221229"
+__githash__ = "5c84ac68ef2e8386"
