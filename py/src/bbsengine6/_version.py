500dc81ebef52a93215fcbbae80608002ac912b6
J (eff) <jam@zoidtechnologies.com>
2026-09-06 14:27:04 -0400

    chore(bbsengine6): stamp _version.py for HEAD (5a3185d)

--- py/src/bbsengine6/_version.py
+++ py/src/bbsengine6/_version.py
@@ -1,3 +1,2 @@
-__version__ = "0.0.1.dev202609061213"
-__datestamp__ = "202609061213-opencode"
-__githash__ = "4ea84c8440873f6c0602f8e528e4652adb053012"
+__version__ = "0.0.1.dev20260906142643"
+__datestamp__ = "20260906-1426-opencode"
